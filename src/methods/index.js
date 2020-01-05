d1cfacf742986cf1887ebc75f1f901840e837876
Frankshi <frankshi@easyops.cn>
2020-01-05 15:03:49 +0800

    feat(tool): add Observe

--- src/methods/index.js
+++ src/methods/index.js
@@ -44,3 +44,17 @@ export const filter = (
   }
   return filteredList;
 };
+
+export class Observe{
+  catchList = [];
+
+  subscribe(fn){
+    this.catchList.push(fn);
+  }
+
+  publish(){
+    for( let i = 0, fn; fn = this.catchList[ i++ ]; ){
+      fn.apply( this, arguments );    // arguments 是发布消息时带上的参数
+    }
+  }
+}
